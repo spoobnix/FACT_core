c80f0638d4a63e058f3868b1762dc9db94b966eb
spoobnix <spooblife@gmail.com>
2020-03-19 13:56:27 -0500

    add todo facade

---
+++ factk/fact.py
@@ -0,0 +1,10 @@
+# TODO: scan site-packages for fact.ext.*
+# TODO: open shelve and add key with the name of the plugin
+# TODO: embed intercom as abstract server, have portal register queue to fact
+# TODO: spawn monolithic client if no itercom registered
+# TODO: handshake with server, register parallel processing if successful
+# TODO: spawn atty if no server instance, act as standalone client
+# TODO: when EPOLL not busy wire to mongodb, create lock on shelve until finished.
+# TODO: create cache after pipeline is succint.
+# TODO: spawn prompt to customize report, ect.
+# TODO: politely shut everything down if finished
