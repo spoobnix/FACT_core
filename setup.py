5641c305f015addefab259ca8cd55ded1527d5c7
spoobnix <spooblife@gmail.com>
2020-03-19 13:09:24 -0500

    todo setup

---
+++ setup.py
@@ -0,0 +1,6 @@
+#TODO: glue tlsh, ssdeep to hashlib.hash
+#TODO: glue pymongo to fact
+#TODO: glue options(radare, docker, qemu, debugger) to fact.ext
+#TODO: refactor fact.portal to use django
+#TODO: glue factk.plugin to fact.ext
+
