3dadf4db1c1049a4838ff9f474ba262f60b2d72f
spoobnix <spooblife@gmail.com>
2020-03-19 13:36:28 -0500

    make plugin manager use flyweight pattern

--- factk/plugin.py
+++ factk/plugin.py
@@ -1,65 +1,43 @@
-import logging
-import os
+from abc import ABC
+import .plugins as Plugin
 
-from common_helper_files import get_binary_from_file, get_dir_of_file, get_files_in_dir
+class AbstractPlugin(ABC):
+    #TODO abstract methods
+    def __prepare__(mcs, bases: *Plugin):
+        try:
+            mcs.register(*bases)
+        except as exc:
+            print('failing gracefully')
+            raise exc
+        finally:
+            return True
 
-from helperFunctions.database import ConnectTo
-from helperFunctions.fileSystem import get_parent_dir
-from storage.db_interface_view_sync import ViewUpdater
+class BasePlugin(AbstractPlugin):
 
+    def __new__(self, *bases, **kwargs):
+        self.DEPS = bases
+        for _, element in enumerate(kwargs):
+            setattr(self, str(_), element)
 
-class BasePlugin():
-    NAME = 'base'
-    DEPENDENCIES = []
+#      def _sync_view(self, plugin_path):
+#       if plugin_path:
+#           view_source = self._get_view_file_path(plugin_path)
+#           if view_source is not None:
+#               view = get_binary_from_file(view_source)
+#               with ConnectTo(ViewUpdater, self.config) as connection:
+#                   connection.update_view(self.NAME, view)
 
-    def __init__(self, plugin_administrator, config=None, plugin_path=None):
-        self.plugin_administrator = plugin_administrator
-        self.config = config
-        self._sync_view(plugin_path)
+#   def _get_view_file_path(self, plugin_path):
+#       plugin_path = get_parent_dir(get_dir_of_file(plugin_path))
+#       view_files = get_files_in_dir(os.path.join(plugin_path, 'view'))
+#       if len(view_files) < 1:
+#           logging.debug('{}: No view available! Generic view will be used.'.format(self.NAME))
+#           return None
+#       if len(view_files) > 1:
+#           logging.warning('{}: Plug-in provides more than one view! \'{}\' is used!'.format(self.NAME, view_files[0]))
+#       return view_files[0]
 
-    def _sync_view(self, plugin_path):
-        if plugin_path:
-            view_source = self._get_view_file_path(plugin_path)
-            if view_source is not None:
-                view = get_binary_from_file(view_source)
-                with ConnectTo(ViewUpdater, self.config) as connection:
-                    connection.update_view(self.NAME, view)
-
-    def _get_view_file_path(self, plugin_path):
-        plugin_path = get_parent_dir(get_dir_of_file(plugin_path))
-        view_files = get_files_in_dir(os.path.join(plugin_path, 'view'))
-        if len(view_files) < 1:
-            logging.debug('{}: No view available! Generic view will be used.'.format(self.NAME))
-            return None
-        if len(view_files) > 1:
-            logging.warning('{}: Plug-in provides more than one view! \'{}\' is used!'.format(self.NAME, view_files[0]))
-        return view_files[0]
-
-    def register_plugin(self):
-        self.plugin_administrator.register_plugin(self.NAME, self)
-import logging
-from pathlib import Path
-
-from common_helper_files import get_dirs_in_dir
-from pluginbase import PluginBase
-
-from helperFunctions.fileSystem import get_src_dir
+#   def register_plugin(self):
+#       self.plugin_administrator.register_plugin(self.NAME, self)
 
 
-def import_plugins(plugin_mount, plugin_base_dir):
-    plugin_base = PluginBase(package=plugin_mount)
-    plugin_src_dirs = _get_plugin_src_dirs(plugin_base_dir)
-    return plugin_base.make_plugin_source(searchpath=plugin_src_dirs)
-
-
-def _get_plugin_src_dirs(base_dir):
-    plug_in_base_path = Path(get_src_dir(), base_dir)
-    plugin_dirs = get_dirs_in_dir(str(plug_in_base_path))
-    plugins = []
-    for plugin_path in plugin_dirs:
-        plugin_code_dir = Path(plugin_path, 'code')
-        if plugin_code_dir.is_dir():
-            plugins.append(str(plugin_code_dir))
-        else:
-            logging.warning('Plugin has no code directory: {}'.format(plugin_path))
-    return plugins
